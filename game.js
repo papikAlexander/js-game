c997def1aebfc50cc3a0ce1be82d730511cfd303
papikAlexander <alexanderpapusha@gmail.com>
2018-09-04 11:38:02 +0300

    add class 'Vector'

--- game.js
+++ game.js
@@ -1,1 +1,20 @@
 'use strict';
+
+class Vector{
+    constructor(x = 0, y = 0){
+        this.x = x, this.y = y;
+    }
+
+    plus(vector){
+        if(!(vector instanceof Vector)){
+            throw new TypeError();
+            
+        } else {
+            return new Vector(this.x + vector.x, this.y + vector.y);
+        }
+    }
+
+    times(number){
+        return new Vector(this.x * number, this.y * number);
+    }
+}
